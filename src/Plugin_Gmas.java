7f790a722f15f0b9965a7235bf7603a3c235700d
juan <jcgaviriaa@zurich.co>
2016-05-16 15:00:51 -0500

    Parametros de usuario

--- src/Plugin_Gmas.java
+++ src/Plugin_Gmas.java
@@ -1,9 +1,9 @@
-import gmas.Estadistica;
 import ij.*;
+import ij.gui.GenericDialog;
 import ij.plugin.*;
+import java.awt.AWTEvent;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
 import java.util.Arrays;
 
 /**
@@ -11,14 +11,25 @@ import java.util.Arrays;
  * @author juan
  */
 public class Plugin_Gmas implements PlugIn {
-    
+
     private double[] data;
-    
+
     @Override
     public void run(String string) {
+        
+        GenericDialog gd = new GenericDialog("");
+        gd.addStringField("Umbralizar", "30", 20);
+        gd.addStringField("Tolerancia grises", "15", 20);
+        gd.showDialog();
+        if (gd.wasCanceled()) {
+            return;
+        }
+        int umbral = Integer.valueOf(gd.getNextString());
+        int tolerancia = Integer.valueOf(gd.getNextString());
+
         ImagePlus impMin = IJ.getImage();
         ImagePlus impMax = IJ.getImage();
-        
+
         ZProjector zpMin = new ZProjector(impMin);
         zpMin.setStartSlice(11);
         zpMin.setStopSlice(20);
@@ -35,13 +46,12 @@ public class Plugin_Gmas implements PlugIn {
         ImagePlus pMax = zpMax.getProjection();
         //pMax.show();
 
-        int umbral = 25;
-        BufferedImage umbralizada = umbralizarPorDelta(umbral, pMin.getBufferedImage(), pMax.getBufferedImage());
+        BufferedImage umbralizada = umbralizarPorDelta(umbral, tolerancia, pMin.getBufferedImage(), pMax.getBufferedImage());
         ImagePlus iumbral = new ImagePlus(String.valueOf(umbral), umbralizada);
         iumbral.show();
     }
 
-    private BufferedImage umbralizarPorDelta(int umbral, BufferedImage bImin, BufferedImage bImax) {
+    private BufferedImage umbralizarPorDelta(int umbral, int tolerancia, BufferedImage bImin, BufferedImage bImax) {
         int width = bImin.getWidth();
         int height = bImin.getHeight();
         int[][] result = new int[width][height];
@@ -52,12 +62,12 @@ public class Plugin_Gmas implements PlugIn {
                 Color c2 = new Color(bImax.getRGB(i, j));
                 int p1 = (c1.getRed() + c1.getGreen() + c1.getBlue()) / 3;
                 int p2 = (c2.getRed() + c2.getGreen() + c2.getBlue()) / 3;
-                if (p2 - p1 > umbral && esGris(c2)/*!esDorado(bImax.getRGB(i, j))*/) {
+                if (p2 - p1 > umbral && esGris(c2, tolerancia)/*!esDorado(bImax.getRGB(i, j))*/) {
                     result[i][j] = bImax.getRGB(i, j);
                 }
             }
         }
-        
+
         //TODO optimizar, se podria hacer en el ciclo anterior
         BufferedImage salida = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         for (int i = 0; i < width; i++) {
@@ -67,13 +77,13 @@ public class Plugin_Gmas implements PlugIn {
         }
         return salida;
     }
-    
-    public boolean esGris(Color color) {
+
+    public boolean esGris(Color color, int tolerancia) {
         prepararEstadistica(color.getRed(), color.getGreen(), color.getBlue());
-        return getDesviacionEstandar() < 16;
+        return getDesviacionEstandar() < tolerancia;
     }
 
-    public void prepararEstadistica(double ... data) {
+    public void prepararEstadistica(double... data) {
         this.data = data;
     }
 
@@ -107,6 +117,4 @@ public class Plugin_Gmas implements PlugIn {
             return data[data.length / 2];
         }
     }
-    
-    
 }
